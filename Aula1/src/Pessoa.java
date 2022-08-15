0963219a8846230b9e493afeb6ccdfb9402ffb1d
GuilhermeNaoki <70299530+naokiota1@users.noreply.github.com>
2022-08-15 11:04:18 -0300

    Add files via upload

---
+++ Aula1/src/Pessoa.java
@@ -0,0 +1,26 @@
+
+public class Pessoa {
+	public static void main(String[] args) {
+		int x = 10;
+		int z = 15;
+		int y;
+		System.out.println("Olá mundo!");
+		System.out.println("x + z = " + (x + z));
+		
+		y = (x + z)/x; 
+		System.out.println("y = " + y);
+		
+		String nome = "Guilherme";
+		String sobrenome = "Ota";
+		System.out.println(nome + " " + sobrenome);
+		
+		System.out.println(nome + x);
+		
+		char letra1 = 'D';
+		char letra2 = 'A';
+		
+		System.out.println(letra1 + letra2);
+		System.out.println(letra1);
+		
+	}
+}
